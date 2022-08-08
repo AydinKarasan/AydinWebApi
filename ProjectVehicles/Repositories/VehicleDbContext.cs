74d46e1041c26ae5e2f1728619ea59d3a1cf89b4
Aydin <Aydin@DESKTOP-1JE5D9Q>
2022-08-08 13:11:52 +0300

    Add project files.

---
+++ ProjectVehicles/Repositories/VehicleDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectVehicles.Models;
+
+namespace ProjectVehicles.Repositories
+{
+    public class VehicleDbContext : DbContext
+    {
+        public VehicleDbContext() { }
+
+        public VehicleDbContext(DbContextOptions<VehicleDbContext> options) : base(options) { }
+
+        public DbSet<Boat> Boats { get; set; }
+        public DbSet<Bus> Buses { get; set; }
+        public DbSet<Car> Cars { get; set; }
+
+    }
+}
